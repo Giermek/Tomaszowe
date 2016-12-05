9ca47769c80845ac585586ad6e3d0f69dbddfba0
Giermek <giermekkrola@gmail.com>
2016-12-05 14:22:19 +0100

    Add files via upload

---
+++ src/app/mock-plans.ts
@@ -0,0 +1,46 @@
+import Plan from './plan';
+
+export const PLANS: Plan[] = [
+  {
+    "id": 0,
+    "name": "labore",
+    "creationDate": "Mon Feb 07 2000 15:04:05 GMT+0100 (CET)",
+    "status": "aute"
+  },
+  {
+    "id": 1,
+    "name": "esse",
+    "creationDate": "Sun Feb 22 1981 02:27:23 GMT+0100 (CET)",
+    "status": "et"
+  },
+  {
+    "id": 2,
+    "name": "sunt",
+    "creationDate": "Thu Aug 09 2012 05:46:28 GMT+0200 (CEST)",
+    "status": "elit"
+  },
+  {
+    "id": 3,
+    "name": "labore",
+    "creationDate": "Wed Apr 06 1977 11:41:47 GMT+0200 (CEST)",
+    "status": "laboris"
+  },
+  {
+    "id": 4,
+    "name": "nostrud",
+    "creationDate": "Sat Apr 19 1986 09:12:20 GMT+0200 (CEST)",
+    "status": "sit"
+  },
+  {
+    "id": 5,
+    "name": "sunt",
+    "creationDate": "Sat Jun 01 1996 13:02:18 GMT+0200 (CEST)",
+    "status": "sint"
+  },
+  {
+    "id": 6,
+    "name": "ut",
+    "creationDate": "Tue May 31 2016 09:41:11 GMT+0200 (CEST)",
+    "status": "labore"
+  }
+];
---
+++ src/app/plan.ts
@@ -0,0 +1,6 @@
+export default class Plan {
+    id: number;
+    name: string;
+    creationDate: string;
+    status: string;
+}
